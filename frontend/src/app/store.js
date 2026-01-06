10e01c3674dc6c82e6034daa784947770a300f54
Mohd Sayam <sayamansari78300@gmail.com>
2026-01-06 13:28:53 +0530

    now i delete redux because i am not able to understand it properly i will go raw now

--- frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-// Import your reducers here as you create them
-// import authReducer from '../features/auth/authSlice';
-
-export const store = configureStore({
-  reducer: {
-    // auth: authReducer, 
-  },
-});
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
-import { store } from "./app/store";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    < Provider store={store}>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
-    </Provider>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
-)
+);
